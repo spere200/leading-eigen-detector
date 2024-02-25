a6dc925d0d67f7643f69c18af260729892549f1e
Samuel Perez <sperez207@gmail.com>
2024-02-25 13:51:15 -0500

    Implemented modularity matrix generation

--- LECD.py
+++ LECD.py
@@ -6,12 +6,15 @@ class LECD:
     def __init__(self, edgeList: np.ndarray) -> None:
         """Leading Eigenvector Community Detector.\n 
         Create an instance by passing it an adjacency matrix and call LECD.split() to perform a split"""
+        self.graphWeight = 0
         self.edgeList = edgeList
         self.nodeMap = defaultdict(int)
         self.nodeList = self._getNodelist()
         self.matrix = self._getMatrix()
+        self.modMatrix = self._getModularityMatrix()
 
-        print(self.matrix)
+        print(self.modMatrix)
+
 
     # generates the list of nodes and maps the nodes to an integer index
     def _getNodelist(self):
@@ -21,6 +24,8 @@ class LECD:
         counter = 0
 
         for edge in self.edgeList:
+            self.graphWeight += edge[2]
+
             if edge[0] not in nodeList:
                 nodeList.add(edge[0])
                 self.nodeMap[edge[0]] = counter
@@ -32,6 +37,21 @@ class LECD:
                 counter += 1
 
         return np.array(list(nodeList))
+    
+    
+    def _getModularityMatrix(self):
+        """Return the modularity matrix of the graph"""
+        modMatrix = np.zeros(self.matrix.shape)
+        
+        for i in range(len(modMatrix)):
+            for j in range(len(modMatrix)):
+                deg1 = np.sum(self.matrix[:,[i]])
+                deg2 = np.sum(self.matrix[:,[j]])
+
+                modMatrix[i][j] = self.matrix[i][j] - ((deg1 * deg2) / (2 * self.graphWeight))
+
+        return modMatrix
+
 
     def _getMatrix(self):
         """Return the adjacency matrix of the graph"""
@@ -44,7 +64,6 @@ class LECD:
             matrix[node2Index][node1Index] = edge[2]
 
         return matrix
-
 
 
 if __name__ == "__main__":
