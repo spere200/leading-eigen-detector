b3461adfc28d721514ef7bb59b34de3e9294a0ba
Samuel Perez <sperez207@gmail.com>
2024-02-26 07:32:44 -0500

    Multiple new changes and bug fixes - Fixed a bug with the index to label map arising from lack of order in sets - Results are now returned in the Split dataclass - Results now include the node list of each group, the edge list of each group, the modularity delta, and the split vector - Results are now returned when manually calling LECD.split() instead of being instantly generated upon initialization

--- LECD.py
+++ LECD.py
@@ -1,11 +1,14 @@
 import numpy as np
 import pandas as pd
 from collections import defaultdict
+from dataclasses import dataclass
 
 class LECD:
     def __init__(self, edgeList: np.ndarray) -> None:
-        """Leading Eigenvector Community Detector.\n 
-        Create an instance by passing it an adjacency matrix and call LECD.split() to perform a split"""
+        """
+        Leading Eigenvector Community Detector.\n 
+        Create an instance by passing it an adjacency matrix and call LECD.split() to perform a split
+        """
         self.graphWeight = 0
         self.edgeList = edgeList
         self.nodeMap = defaultdict(int)
@@ -13,26 +16,34 @@ class LECD:
         self.matrix = self._getMatrix()
         self.modMatrix = self._getModularityMatrix()
 
-        self.groups = defaultdict(list[str])
-        splitVector = self.split()
-
-        for g, v in self.groups.items():
-            print(g, v)
-
-        print(f"Delta: {self.getDelta(splitVector)}")
-
-
     def split(self):
+        """
+        Performs the split using the leading eigenvector of the modularity matrix generated on initialization.\n
+        """
         _, eigenvectors = np.linalg.eig(self.modMatrix)
         resultVector = eigenvectors.T[0]
+        groups = [[], []]
+        graphs = [[], []]
 
         for i, val in enumerate(resultVector):
             if val < 0:
-                self.groups[0].append(self.nodeList[i])
-            else:
-                self.groups[1].append(self.nodeList[i])
+                for tar in groups[0]:
+                    tarIndex = self.nodeMap[tar]
 
-        return resultVector
+                    if self.matrix[i][tarIndex] > 0:
+                        graphs[0].append([self.nodeList[i], tar, self.matrix[i][tarIndex]])
+
+                groups[0].append(self.nodeList[i])
+            else:
+                for tar in groups[1]:
+                    tarIndex = self.nodeMap[tar]
+
+                    if self.matrix[i][tarIndex] > 0:
+                        graphs[1].append([self.nodeList[i], tar, self.matrix[i][tarIndex]])
+
+                groups[1].append(self.nodeList[i])
+        
+        return Split(groups, graphs, self.getDelta(resultVector), resultVector)
 
 
     def getDelta(self, splitVector):
@@ -40,31 +51,32 @@ class LECD:
         finalProd = np.matmul(firstProd, splitVector)
 
         return (finalProd) / (4 * self.graphWeight)
-
-        # delta = (splitVector.T * ) / (4 * self.graphWeight)
         
 
     # generates the list of nodes and maps the nodes to an integer index
     def _getNodelist(self):
         """Return the list of unique nodes of the graph"""
-        nodeList = set()
+        seen = set()
+        nodeList = []
 
         counter = 0
 
         for edge in self.edgeList:
             self.graphWeight += edge[2]
 
-            if edge[0] not in nodeList:
-                nodeList.add(edge[0])
+            if edge[0] not in seen:
+                nodeList.append(edge[0])
+                seen.add(edge[0])
                 self.nodeMap[edge[0]] = counter
                 counter += 1
 
-            if edge[1] not in nodeList:
-                nodeList.add(edge[1])
+            if edge[1] not in seen:
+                nodeList.append(edge[1])
+                seen.add(edge[1])
                 self.nodeMap[edge[1]] = counter
                 counter += 1
 
-        return np.array(list(nodeList))
+        return nodeList
     
     
     def _getModularityMatrix(self):
@@ -83,7 +95,7 @@ class LECD:
 
     def _getMatrix(self):
         """Return the adjacency matrix of the graph"""
-        matrix = np.zeros((self.nodeList.size, self.nodeList.size))
+        matrix = np.zeros((len(self.nodeList), len(self.nodeList)))
 
         for edge in self.edgeList:
             node1Index = self.nodeMap[edge[0]]
@@ -93,7 +105,27 @@ class LECD:
 
         return matrix
 
+@dataclass
+class Split:
+    """
+    Split.groups contains two lists, where each list has the members by labels of each group.\n
+    Split.graphs contains two lists, where each list contains all the edges of each group.\n
+    Split.delta contains the change in modularity after performing this split.\n
+    Split._splitVector contains the vector that was used to split the group.
+    """
+    groups: list[list[str]]
+    graphs: list[list]
+    delta: float
+    _splitVector: list[float]
 
+    def __str__(self) -> str:
+        return f"group1: {self.groups[0]}\ngroup2: {self.groups[1]}\ndelta: {self.delta}"
+
+# used for testing
 if __name__ == "__main__":
     edgeList = np.array(pd.read_csv('small.csv'))
-    test = LECD(edgeList)
+    lecd = LECD(edgeList)
+    result = lecd.split()
+
+    for row in result.graphs[1]:
+        print(row)
