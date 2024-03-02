9edcf23b797ed823d3bb786f70760ee2557e46ac
Samuel Perez <sperez207@gmail.com>
2024-03-01 23:58:48 -0500

    Added visualization with pyvis and networkX Uses can now create visualizations in an html file using pyvis, which colors separate groups different colors, and lets users interact with the network.

--- Detector.py
+++ Detector.py
@@ -1,19 +1,23 @@
 import pandas as pd
 import numpy as np
+from pyvis.network import Network
+import networkx as nx
+
 from dataclasses import dataclass
 import logging
 import time
 
 from LECD import LECD
 
-class Detector:
-    def __init__(self, edgeList, log=False) -> None:
-        if log:
-            logging.basicConfig(level=logging.INFO, format='INFO: %(message)s')
 
+class Detector:
+    def __init__(self, edgeList) -> None:
         self.edgeList = edgeList
 
-    def getLECommunities(self, xls=True):
+    def getLECommunities(self, xlsx=True, visualize=False, log=False):
+        if log:
+            logging.basicConfig(level=logging.INFO, format="INFO: %(message)s")
+
         groups = []
         graphs = []
 
@@ -45,29 +49,72 @@ class Detector:
 
         dfs(self.edgeList)
 
-        if xls:
-            logging.info("")
-            logging.info(f"Generating the output .xlsx file for a graph with {len(groups)} groups...")
+        results = DetectorResult(groups, graphs)
 
-            with pd.ExcelWriter("output_groups.xlsx", engine='xlsxwriter') as writer:
+        if xlsx:
+            logging.info("")
+            logging.info(
+                f"Generating the output.xlsx file for a graph with {len(groups)} groups..."
+            )
+
+            with pd.ExcelWriter("output.xlsx", engine="xlsxwriter") as writer:
                 # writing the groups with group labels to the first sheet of the document
                 groupHeaders = [f"group{i}" for i in range(len(groups))]
                 df = pd.DataFrame(groups)
                 df = df.transpose()
                 df.columns = groupHeaders
-                logging.info(f"  Creating the \"groups\" sheet...")
+                logging.info(f'  Creating the "groups" sheet...')
                 df.to_excel(writer, sheet_name="groups", index=False)
 
                 for i, row in enumerate(graphs):
-                    logging.info(f"  Creating the \"group{i}_edges\" sheet...")
+                    logging.info(f'  Creating the "group{i}_edges" sheet...')
                     df = pd.DataFrame(row)
                     df.columns = ["node1", "node2", "weight"]
                     df.to_excel(writer, sheet_name=f"group{i}_edges", index=False)
 
+        if visualize:
+            logging.info(f"")
+            logging.info(f"Creating the output.html file for the visualization")
+
+            nxGraph = nx.Graph()
+
+            groupID = 0
+
+            for group in results.groups:
+                for node in group:
+                    nxGraph.add_node(
+                        node,
+                        size=15,
+                        title=f"group {groupID}",
+                        label=node,
+                        group=groupID,
+                    )
+
+                groupID += 1
+
+            for graph in results.graphs:
+                nxGraph.add_weighted_edges_from(graph)
+
+            net = Network()
+            net.from_nx(nxGraph)
+            net.show_buttons(filter_=["physics"])
+
+            net.barnes_hut(
+                gravity=-500,
+                central_gravity=0.3,
+                spring_length=90,
+                spring_strength=0.04,
+                damping=0.09,
+                overlap=0,
+            )
+
+            net.toggle_physics(False)
+            net.show("output.html", notebook=False)
+
         logging.info(f"")
         logging.info(f"Done after {(time.time() - processStartTime):.6f} seconds.")
 
-        return DetectorResult(groups, graphs)
+        return results
 
 
 @dataclass
@@ -75,6 +122,7 @@ class DetectorResult:
     """
     Wrapper dataclass for the results, it just includes the list of groups and the list of edges for each group of nodes.
     """
+
     groups: list[list[str]]
     graphs: list[list]
 
--- test.py
+++ test.py
@@ -1,29 +1,12 @@
 import numpy as np
 import pandas as pd
-# import networkx as nx
-# import matplotlib.pyplot as plt
 
 from Detector import Detector
 
 # used for testing
 if __name__ == "__main__":
-    edgeList = np.array(pd.read_csv("data/COAD.csv"))
-
-    # # this was only used to generate the sample graph for the .md file, feel  
-    # # free to add it back in if you want but you'll have to install matplotlib 
-    # # networkx, and scipy, and it's extremely impractical for very large graphs
-    # G = nx.Graph()
-    # G.add_weighted_edges_from(edgeList)
-
-    # pos = nx.spring_layout(G)
-    # nx.draw(G, pos, with_labels=True, node_color="skyblue", node_size=700, font_size=14)
-    # edge_labels = nx.get_edge_attributes(G, 'weight')
-    # nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=12)
-
-    # plt.show()
-
-    detector = Detector(edgeList, log=True)
-    results = detector.getLECommunities()
-
-    # for row in results.groups:
-    #     print(row)
+    edgeList = np.array(pd.read_csv("data/small.csv"))
+    detector = Detector(edgeList)
+    results = detector.getLECommunities(
+        log=True, xlsx=True, visualize=True  # just delete all args to set all to false
+    )
